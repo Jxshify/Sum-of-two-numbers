16704c77c743d1ab28de50c204777079665d117b
Joshua <127119693+Jxshify@users.noreply.github.com>
2023-03-06 22:36:57 +0800

    Add files via upload

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int x, y;
+    printf("This program adds 2 numbers together.\n");
+    printf("Enter first number: ");
+    scanf("%d", &x);
+    printf("Enter second number: ");
+    scanf("%d", &y);
+    printf("The sum of two numbers is %d", x+y);
+}
